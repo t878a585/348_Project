868bf3a3e4008c48edce70cca77bc62bc5a74ff4
allie1587 <130801516+allie1587@users.noreply.github.com>
2023-11-15 15:34:28 -0600

    Update Token.hpp

--- Src/Token.hpp
+++ Src/Token.hpp
@@ -1,4 +1,33 @@
 #ifndef TOKEN_HPP
 #define TOKEN_HPP
+class Token {
+        private:
+                //Is true if operator, false if operand
+                bool is_Operator;
+                long double operand_Value;
+                char operator_Value;
 
+        public:
+
+                Token (long double _operand_Value) {
+                        is_Operator = false;
+                        operand_Value = _operand_Value;
+                }
+
+                Token (char _operator_Value) {
+                        is_Operator = true;
+                        operator_Value = _operator_Value;
+                }
+
+                bool is_This_An_Operator() {
+                        return is_Operator;
+                }
+
+		char get_Operator() { return operator_Value; }
+
+		long double get_Operand() { return operand_Value; }
+};
+
+
+#endif
 #endif
