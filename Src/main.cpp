a656fc83d3efa18d6deeff193bb06f8f849e3131
allie1587 <130801516+allie1587@users.noreply.github.com>
2023-12-02 09:59:37 -0600

    Update main.cpp

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,10 +4,15 @@
  * Timo Aranjo, Victor Maduka, Ellia Morse, Deborah Onuosa
  */
 
+//Include the header file "IOHandler.hpp" to makek the declarations in IOHandler.hpp avaialbe for use in this file
 #include "IOHandler.hpp"
 
+//The main function, where the execution begins
 int main() {
-	IOHandler ioHandler;
-	ioHandler.execute();
-	return 0;
+  //Creates an instance of the IOHandler class named ioHandler
+  IOHandler ioHandler; 
+  //Calls the execute() method of the ioHandler instance, which prompts the user for input and displays the output
+  ioHandler.execute();
+  //Return 0 to indicate successful execution
+  return 0;
 }
